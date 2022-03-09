ab25780bb85d8cc472549ef10de1b08cc0b0c28e
Xhunik Miguel <xhuniktzi@gmail.com>
2022-03-08 21:18:45 -0600

    fase 2 inicial

---
+++ Fase2/src/Edd/Proyecto/Fase2/Fase2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Edd.Proyecto.Fase2;
+
+/**
+ *
+ * @author Xhunik_Local
+ */
+public class Fase2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

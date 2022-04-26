e354bc35dbf5a964fa8ebf092bffa82c4ccdfdf5
Xhunik Miguel <xhuniktzi@gmail.com>
2022-04-26 15:45:10 -0600

    init fase 3

---
+++ Fase3/src/fase3/Fase3.java
@@ -0,0 +1,16 @@
+package fase3;
+
+/**
+ *
+ * @author Xhunik
+ */
+public class Fase3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Fase3/src/fase3/models/Cliente.java
@@ -0,0 +1,16 @@
+package fase3.models;
+
+/**
+ *
+ * @author Xhunik
+ */
+public class Cliente {
+    public String dpi;
+    public String name;
+    public String username;
+    public String mail;
+    public String password;
+    public String phone;
+    public String direction;
+    public long id_municipio;
+}
